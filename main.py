03a57bba2ada8b4db8394abfd7a125d80f8a4df3
eovillafuerte <eovillafuerte@outlook.com>
2022-06-27 13:03:58 -0600

    api set up

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "76aa0d3f7a72b814b612b6f4532811bc"
+
+weather_params = {
+    "lat": 39.575500,
+    "lon": -106.100400,
+    "appid": api_key,
+}
+
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.status_code)
